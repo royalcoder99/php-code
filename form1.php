29d61b25c2ef33d01e2abb684663706e31c47d5d
Pankaj Ojha <84920941+pankaj250483@users.noreply.github.com>
2023-03-27 13:16:15 +0530

    update

---
+++ form1.php
@@ -0,0 +1,17 @@
+<?php
+
+$name=$_GET['name'];
+$city=$_GET['city'];
+
+echo "<h1>Hello! I am ".$name."</h1>";
+echo "<p>I live in ".$city."</p>";
+
+?>
+
+<p> hello this is test</p>
+
+<?php
+
+echo "this is global city variablae value is $city";
+
+?>
